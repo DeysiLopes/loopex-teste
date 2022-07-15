effaf71c18b5b913e45fba5831a674a22be22f6b
DeysiLopes <deysilinconl@gmail.com>
2022-07-15 18:51:38 -0300

    remove element of the tree

--- src/loopex/teste/arvore/Arvore.java
+++ src/loopex/teste/arvore/Arvore.java
@@ -62,5 +62,92 @@ public class Arvore<T extends Comparable> {
     	}
     }
 
+    public boolean remover(T valor){
+        Elemento<T> atual = this.raiz;
+        Elemento<T> paiAtual = null;
+        while(atual != null){
+            if (atual.getValor().equals(valor)){
+                break;                
+            }else if (valor.compareTo(atual.getValor()) == -1){ 
+                paiAtual = atual;
+                atual = atual.getEsquerdo();
+            }else{
+                paiAtual = atual;
+                atual = atual.getDireito();
+            }
+        }
+        
+        if (atual != null){
+            
+            
+            if (atual.getDireito() != null){
+                
+                Elemento<T> substituto = atual.getDireito();
+                Elemento<T> paiSubstituto = atual;
+                while(substituto.getEsquerdo() != null){
+                    paiSubstituto = substituto;
+                    substituto = substituto.getEsquerdo();
+                }
+                substituto.setEsquerdo(atual.getEsquerdo());
+                if (paiAtual != null){
+                    if (atual.getValor().compareTo(paiAtual.getValor()) == -1){ //atual < paiAtual
+                        paiAtual.setEsquerdo(substituto);
+                    }else{
+                        paiAtual.setDireito(substituto);
+                    }
+                }else{ 
+                    this.raiz = substituto;
+                    paiSubstituto.setEsquerdo(null);
+                    this.raiz.setDireito(paiSubstituto);
+                    this.raiz.setEsquerdo(atual.getEsquerdo());
+                }
+                
+              
+                if (substituto.getValor().compareTo(paiSubstituto.getValor()) == -1){ //substituto < paiSubstituto
+                    paiSubstituto.setEsquerdo(null);
+                }else{
+                    paiSubstituto.setDireito(null);
+                }
+                
+            }else if (atual.getEsquerdo() != null){ 
+                Elemento<T> substituto = atual.getEsquerdo();
+                Elemento<T> paiSubstituto = atual;
+                while(substituto.getDireito() != null){
+                    paiSubstituto = substituto;
+                    substituto = substituto.getDireito();
+                }
+                if (paiAtual != null){
+                    if (atual.getValor().compareTo(paiAtual.getValor()) == -1){
+                        paiAtual.setEsquerdo(substituto);
+                    }else{
+                        paiAtual.setDireito(substituto);
+                    }
+                }else{ 
+                    this.raiz = substituto;
+                }
+                
+               
+                if (substituto.getValor().compareTo(paiSubstituto.getValor()) == -1){ 
+                    paiSubstituto.setEsquerdo(null);
+                }else{
+                    paiSubstituto.setDireito(null);
+                }
+            }else{
+                if (paiAtual != null){
+                    if (atual.getValor().compareTo(paiAtual.getValor()) == -1){ 
+                        paiAtual.setEsquerdo(null);
+                    }else{
+                        paiAtual.setDireito(null);
+                    }
+                }else{ 
+                    this.raiz = null;
+                }
+            }
+            
+            return true;
+        }else{
+            return false;
+        }        
+    }
     
 }
